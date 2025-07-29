364281cdda994dff363a360c4271571fbe6ee7df
Alagu.BalaGanapathy <Alagu.BalaGanapathy@cdw.com>
2025-07-29 12:27:52 +0530

    final

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,7 @@ app.get('/', (_req, res) => {
 if (process.env.NODE_ENV !== 'test') {
   const PORT = process.env.PORT || 3000;
   app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+    console.log(`Server is running on port by Alagu ${PORT}`);
   });
 }
 
